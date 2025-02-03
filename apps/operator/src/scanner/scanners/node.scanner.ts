bc73a9582a34d24483b91bb83021e28cacbf171e
Daniel Kisselman <daniel.kisselman@xmcyber.com>
2025-02-03 18:25:10 +0200

    Fixing some logic

--- apps/operator/src/scanner/scanners/node.scanner.ts
+++ apps/operator/src/scanner/scanners/node.scanner.ts
@@ -5,17 +5,9 @@ import { KubeService } from '../../kube/kube.service';
 import { ConfigService } from '../../config/config.service';
 import { enrichKubernetesObject } from '../../utils/kube';
 
-interface NodeDetails {
-  nodeName: string;
-  totalPods: number;
-  daemonSets: Set<string>;
-}
-
 @Injectable()
 export class NodeScanner extends BaseResourceScanner<k8s.V1Node> {
-  private allPodsinCluster: Map<string ,k8s.V1Pod>;
-  private allNodesinCluster: k8s.V1NodeList;
-  private nodeDetailsMap = new Map<string, NodeDetails>();
+  private usedNodes = new Map<string, Boolean>();
 
   constructor(private readonly kubeService: KubeService, config: ConfigService) {
     super(config);
@@ -23,23 +15,21 @@ export class NodeScanner extends BaseResourceScanner<k8s.V1Node> {
   
   async preScan(): Promise<void> {
       const pods = await this.kubeService.coreApi.listPodForAllNamespaces();
-      this.allPodsinCluster = new Map(pods.items.map((pod) => [pod.metadata!.name!, pod]));
+      pods.items.forEach((pod) => {
+        const nodeName = pod.spec?.nodeName;
+        const ownerReferences = pod.metadata?.ownerReferences;
+        ownerReferences?.forEach((ref) => {
+          if (ref.kind !== "DaemonSet") {
+            this.usedNodes.set(nodeName!, true);
+          }
+        })
+      })
   }
 
   async scan(): Promise<k8s.V1Node[]> {
     try {
-      const response = (await this.kubeService.coreApi.listNode());
-      const nodes = response.items;
-      nodes.map((node) => enrichKubernetesObject(node, 'Node'))
-        .forEach((node) => {
-        this.nodeDetailsMap.set(node.metadata!.name!, {
-          nodeName: node.metadata!.name!,
-          totalPods: 0,
-          daemonSets: new Set<string>(),
-        })
-      })
-      console.log(nodes)
-      return nodes; 
+      return (await this.kubeService.coreApi.listNode()).items
+      .map((node) => enrichKubernetesObject(node, 'Node') as k8s.V1Node); 
     } catch (error) {
       this.logger.error(`Failed to scan Node: ${error.message}`);
       throw error;
@@ -47,38 +37,9 @@ export class NodeScanner extends BaseResourceScanner<k8s.V1Node> {
   }
 
   async isOrphaned(node: k8s.V1Node): Promise<{ isOrphaned: boolean; reason?: string }> {
-    try {
-      const pods = this.allPodsinCluster;
-      const nodeName = node.metadata!.name!;
-      const nodeDetails = this.nodeDetailsMap.get(nodeName);
-
-      pods.forEach((pod) => {
-        if (nodeName && this.nodeDetailsMap.has(nodeName)) {
-          if (pod.spec.nodeName === nodeName) {
-            nodeDetails!.totalPods++;
-          }
-          const ownerReferences = pod.metadata?.ownerReferences;
-          ownerReferences!.forEach((ref) => {
-            if (ref.kind === "DaemonSet") {
-              nodeDetails!.daemonSets.add(ref.name!);
-            }
-          })
-        }
-      })
-      if (this.nodeDetailsMap.get(node.metadata!.name!)?.totalPods === 0) {
-        return {
-          isOrphaned: true,
-          reason: "Node is not running any workloads",
-        };
-      }
-      const hasOnlyDaemonSets = nodeDetails.totalPods > 0 && nodeDetails.totalPods === nodeDetails.daemonSets.size;
-      console.log(hasOnlyDaemonSets);
-      return {
-        isOrphaned: hasOnlyDaemonSets,
-        reason: "Nodes are not running any workloads besides DaemonSets",
-      };
-    } catch (error) { 
-      console.log(error);
-    }
+    return {
+      isOrphaned: !this.usedNodes.has(node.metadata.name!),
+      reason: 'Node is not used by any workloads, except DaemonSets',
+    };
   }
 }
